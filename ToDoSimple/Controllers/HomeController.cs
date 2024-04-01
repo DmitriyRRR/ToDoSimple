89020e03c118e8255a87302a85c7a1757319ad96
DmitriyRRR <romanov.dmitriy.ua@gmail.com>
2024-04-01 23:38:58 +0200

    Add project files.

---
+++ ToDoSimple/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ToDoSimple.Models;
+
+namespace ToDoSimple.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Create(string name, string discription)
+        {
+            Note note = new Note();
+            note.Name = name;
+            note.Description = discription;
+            return View(note);
+        }
+        public async Task<IActionResult> Delete(string id) //реализовать
+        {
+            return RedirectToAction("Index");
+        }
+        
+        public async Task<IActionResult> Edite(string id) //реализовать
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ ToDoSimple/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoSimple.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}

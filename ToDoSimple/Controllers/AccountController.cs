cdf264f9b4cae66883e6609b76f8eaddd8c2baa4
Dmitriy Romanov <romanov.dmitriy.ua@gmail.com>
2024-04-14 22:12:30 +0200

    ...Not all

---
+++ ToDoSimple/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoSimple.Models.Account;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+
+namespace ToDoSimple.Controllers
+{
+    public class AccountController : Controller
+
+    {
+        private readonly ToDoContext _context;
+        protected int _accountId => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        public AccountController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> LogoutAsync(LoginViewModel model)
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}

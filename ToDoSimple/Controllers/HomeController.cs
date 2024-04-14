779e292380cc44b820fcfa3d8f813415fae1bbd7
Dmitriy Romanov <romanov.dmitriy.ua@gmail.com>
2024-04-14 18:42:22 +0200

    Add Auth toHomeController

--- ToDoSimple/Controllers/HomeController.cs
+++ ToDoSimple/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using ToDoSimple.Models;
@@ -6,6 +7,7 @@ using ToDoSimple.Models.Home;
 
 namespace ToDoSimple.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
